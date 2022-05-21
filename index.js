fd7bc2d9ea5710b8bda3f8d1c35d1bc6b38d2a32
ahmad <waqas.ahmad00@yahoo.com>
2022-05-21 14:48:53 +0500

    checking

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node web app!\n'
   res.end(msg);
 });
+
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
